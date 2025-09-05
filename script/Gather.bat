4a0bbd13467857d7a01b8e3f871271a66847f34e
Mr.Han <57278320+mr-pangpang@users.noreply.github.com>
2025-09-06 03:32:24 +0800

    Update Gather.bat

--- script/Gather.bat
+++ script/Gather.bat
@@ -6,16 +6,12 @@ set "original_url=https://tv-1.iill.top/m3u/Gather"
 
 :: 直接获取URL内容并显示
 echo 正在获取URL内容...
-echo.
-echo ==================== 内容开始 ====================
-echo.
 
 :: 使用curl获取内容并直接输出到控制台
 curl -L "%original_url%"
 
 if !errorlevel! equ 0 (
     echo.
-    echo ==================== 内容结束 ====================
     echo 获取成功！
 ) else (
     echo.
